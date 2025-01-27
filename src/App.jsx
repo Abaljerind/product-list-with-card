5edc7d6fc06737ca4f9252066e1298a70308d432
Abaljerind <abaljerinda@gmail.com>
2025-01-27 22:25:53 +0700

    Create a static UI for only waffle card
    Import icon add to cart and image waffle mobile to App.jsx
    Change background color for body
    Add new elements with each styles based on image of mobile design empty
    Add h1, div with content of img, button, small and 2 tags p

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import cart from "./assets/images/icon-add-to-cart.svg";
+import waffleMobile from "./assets/images/image-waffle-mobile.jpg";
+
 function App() {
+  document.body.style.backgroundColor = "rgb(254, 250, 247)";
+
   return (
     <>
-      <h1 class="font-red-hat text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="font-red-hat my-8 ml-6 text-5xl font-bold text-rose-900">
+        Desserts
+      </h1>
+      <div className="font-red-hat mx-auto mb-4 flex w-[335px] flex-col justify-center">
+        <img
+          src={waffleMobile}
+          alt="Waffle with Berries"
+          className="rounded-[12px]"
+        />
+        <button className="mx-auto -mt-6 mb-4 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900">
+          <img src={cart} alt="Add to cart icon" className="scale-110" /> Add to
+          Cart
+        </button>
+        <small className="text-[16px] font-medium text-rose-500">Waffle</small>
+        <p className="text-lg font-semibold text-rose-900">
+          Waffle with Berries
+        </p>
+        <p className="text-reds text-lg font-semibold">$6.50</p>
+      </div>
     </>
   );
 }
