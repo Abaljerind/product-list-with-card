7f2da8f70245533f27ed955b4d8d81dd792f299f
Abaljerind <abaljerinda@gmail.com>
2025-01-28 12:05:17 +0700

    Import foodData from data.json and save it as initial value for data state
    Re-place style for body outside of App()
    Create state for data with initial value foodData
    Mapping data state and return it with div from before
    Add key to div based on index + 1
    Change all the statics data with dynamics data from data state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import cart from "./assets/images/icon-add-to-cart.svg";
-import waffleMobile from "./assets/images/image-waffle-mobile.jpg";
+import { useState } from "react";
+import foodData from "../data.json";
+import cart from "/images/icon-add-to-cart.svg";
 
+document.body.style.backgroundColor = "rgb(254, 250, 247)";
 function App() {
-  document.body.style.backgroundColor = "rgb(254, 250, 247)";
+  const [data, setData] = useState(foodData);
 
   return (
     <>
       <h1 className="font-red-hat my-8 ml-6 text-5xl font-bold text-rose-900">
         Desserts
       </h1>
-      <div className="font-red-hat mx-auto mb-4 flex w-[335px] flex-col justify-center">
-        <img
-          src={waffleMobile}
-          alt="Waffle with Berries"
-          className="rounded-[12px]"
-        />
-        <button className="mx-auto -mt-6 mb-4 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900">
-          <img src={cart} alt="Add to cart icon" className="scale-110" /> Add to
-          Cart
-        </button>
-        <small className="text-[16px] font-medium text-rose-500">Waffle</small>
-        <p className="text-lg font-semibold text-rose-900">
-          Waffle with Berries
-        </p>
-        <p className="text-reds text-lg font-semibold">$6.50</p>
-      </div>
+      {data.map((food, index) => {
+        return (
+          <div
+            className="font-red-hat mx-auto mb-6 flex w-[335px] flex-col justify-center"
+            key={index + 1}
+          >
+            <img
+              src={food.image.mobile}
+              alt={food.name}
+              className="rounded-[12px]"
+            />
+            <button className="mx-auto -mt-6 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900">
+              <img src={cart} alt="Add to cart icon" className="scale-110" />{" "}
+              Add to Cart
+            </button>
+            <small className="mt-4 text-[16px] font-medium text-rose-500">
+              {food.category}
+            </small>
+            <p className="text-lg font-semibold text-rose-900">{food.name}</p>
+            <p className="text-reds text-lg font-semibold">${food.price}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
