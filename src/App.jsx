e87046317667a14441244ad5a284894dd815a821
Abaljerind <abaljerinda@gmail.com>
2025-01-28 15:28:30 +0700

    Create empty cart card and give it some styles
    Empty cart div consist of h3, div with contents of img and tag p

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import foodData from "../data.json";
 import cart from "/images/icon-add-to-cart.svg";
+import emptyCart from "/images/illustration-empty-cart.svg";
 
 document.body.style.backgroundColor = "rgb(254, 250, 247)";
 function App() {
@@ -34,6 +35,18 @@ function App() {
           </div>
         );
       })}
+
+      <div className="font-red-hat mx-auto mt-6 mb-6 flex w-[335px] flex-col justify-center rounded-2xl bg-white">
+        <h3 className="text-reds mt-7 ml-6 text-2xl font-bold">
+          Your Cart (0)
+        </h3>
+        <div className="mt-8 mb-6 flex flex-col items-center justify-center">
+          <img src={emptyCart} alt="Illustration of Empty Cart" />
+          <p className="mt-3 text-[16px] font-semibold text-rose-500">
+            Your added items will appear here
+          </p>
+        </div>
+      </div>
     </>
   );
 }
