00f5e03b635b01f3d3737cdb59513d1b6a409563
Abaljerind <abaljerinda@gmail.com>
2025-01-30 21:30:26 +0700

    Create component AddFood for user increase or decrease the food
    Send selectedFood, increment and decrement from AddFood instance component as props
    AddFood receive them as props

--- src/App.jsx
+++ src/App.jsx
@@ -274,23 +274,11 @@ function FoodList({ food, selectedFood, increment, decrement, onAddFood }) {
     <div className="font-red-hat mx-auto mb-6 flex w-[335px] flex-col justify-center">
       <img src={food.image.mobile} alt={food.name} className="rounded-[12px]" />
       {selectedFood ? (
-        <div className="bg-reds mx-auto -mt-[30px] flex w-44 items-center justify-between gap-2 rounded-full px-3 py-3 font-semibold text-white">
-          <button onClick={() => decrement(selectedFood)}>
-            <img
-              src={minus}
-              alt="Icon decrement"
-              className="h-8 w-8 scale-[60%] rounded-full border-2 border-white object-contain p-1"
-            />
-          </button>
-          <p>{selectedFood?.quantity || 0}</p>
-          <button onClick={() => increment(selectedFood)}>
-            <img
-              src={plus}
-              alt="Icon decrement"
-              className="h-8 w-8 scale-[60%] rounded-full border-2 border-white object-contain p-1"
-            />
-          </button>
-        </div>
+        <AddFood
+          selectedFood={selectedFood}
+          increment={increment}
+          decrement={decrement}
+        />
       ) : (
         <button
           className="mx-auto -mt-6 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900"
@@ -306,6 +294,28 @@ function FoodList({ food, selectedFood, increment, decrement, onAddFood }) {
       </small>
       <p className="text-lg font-semibold text-rose-900">{food.name}</p>
       <p className="text-reds text-lg font-semibold">${food.price}</p>
+    </div>
+  );
+}
+
+function AddFood({ selectedFood, increment, decrement }) {
+  return (
+    <div className="bg-reds mx-auto -mt-[30px] flex w-44 items-center justify-between gap-2 rounded-full px-3 py-3 font-semibold text-white">
+      <button onClick={() => decrement(selectedFood)}>
+        <img
+          src={minus}
+          alt="Icon decrement"
+          className="h-8 w-8 scale-[60%] rounded-full border-2 border-white object-contain p-1"
+        />
+      </button>
+      <p>{selectedFood?.quantity || 0}</p>
+      <button onClick={() => increment(selectedFood)}>
+        <img
+          src={plus}
+          alt="Icon decrement"
+          className="h-8 w-8 scale-[60%] rounded-full border-2 border-white object-contain p-1"
+        />
+      </button>
     </div>
   );
 }
