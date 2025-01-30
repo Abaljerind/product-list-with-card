e9ac390ff05611a065655dced5c7b3813df160be
Abaljerind <abaljerinda@gmail.com>
2025-01-30 22:37:19 +0700

    Create component ListFoodInsideCart
    ListFoodInsideCart instance send props priceFoods, selectedFood, food and onDeleteFood
    ListFoodInsideCart receive it

--- src/App.jsx
+++ src/App.jsx
@@ -270,37 +270,16 @@ function CartList({ totalFood, totalPrice, cart, onDeleteFood }) {
         );
 
         return (
-          <div
-            className="mr-6 mb-6 ml-6 flex justify-between border-b border-rose-100 pb-4"
+          <ListFoodInsideCart
             key={index + 1}
-          >
-            <div className="flex flex-col">
-              <p className="text-[16px] font-semibold text-rose-900">
-                {food.name}
-              </p>
-              <small className="mt-2 text-[16px]">
-                <span className="text-reds font-semibold">
-                  {food.quantity}x
-                </span>
-                <span className="ml-4 font-medium text-rose-400">
-                  @ ${Number(food.price).toFixed(2)}
-                </span>
-                <span className="ml-1 font-semibold text-rose-500">
-                  {" "}
-                  ${Number(priceFoods).toFixed(2)}
-                </span>
-              </small>
-            </div>
-            <button onClick={() => onDeleteFood(selectedFood)}>
-              <img
-                src={iconRemove}
-                alt="icon remove"
-                className="h-8 w-8 scale-[60%] rounded-full border-2 border-rose-300 object-contain p-1"
-              />
-            </button>
-          </div>
+            priceFoods={priceFoods}
+            selectedFood={selectedFood}
+            food={food}
+            onDeleteFood={onDeleteFood}
+          />
         );
       })}
+
       <div className="mr-6 mb-6 ml-6 flex items-center justify-between">
         <p className="text-[16px] font-semibold text-rose-500">Order Total</p>
         <p className="text-[28px] font-bold text-rose-900">
@@ -317,6 +296,33 @@ function CartList({ totalFood, totalPrice, cart, onDeleteFood }) {
       </div>
       <button className="bg-reds mx-auto mb-6 w-[85%] rounded-full py-4 font-semibold text-white">
         Confirm Order
+      </button>
+    </div>
+  );
+}
+
+function ListFoodInsideCart({ priceFoods, selectedFood, food, onDeleteFood }) {
+  return (
+    <div className="mr-6 mb-6 ml-6 flex justify-between border-b border-rose-100 pb-4">
+      <div className="flex flex-col">
+        <p className="text-[16px] font-semibold text-rose-900">{food.name}</p>
+        <small className="mt-2 text-[16px]">
+          <span className="text-reds font-semibold">{food.quantity}x</span>
+          <span className="ml-4 font-medium text-rose-400">
+            @ ${Number(food.price).toFixed(2)}
+          </span>
+          <span className="ml-1 font-semibold text-rose-500">
+            {" "}
+            ${Number(priceFoods).toFixed(2)}
+          </span>
+        </small>
+      </div>
+      <button onClick={() => onDeleteFood(selectedFood)}>
+        <img
+          src={iconRemove}
+          alt="icon remove"
+          className="h-8 w-8 scale-[60%] rounded-full border-2 border-rose-300 object-contain p-1"
+        />
       </button>
     </div>
   );
