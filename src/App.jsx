8cd841009a2475599e48f5109500f355d33a636a
Abaljerind <abaljerinda@gmail.com>
2025-01-30 21:33:43 +0700

    Create component AddToCart for button add to cart
    Send food and onAddFood as props from AddToCart instance
    AddToCart receive them as props

--- src/App.jsx
+++ src/App.jsx
@@ -280,13 +280,7 @@ function FoodList({ food, selectedFood, increment, decrement, onAddFood }) {
           decrement={decrement}
         />
       ) : (
-        <button
-          className="mx-auto -mt-6 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900"
-          onClick={() => onAddFood(food)}
-        >
-          <img src={cartIcon} alt="Add to cart icon" className="scale-110" />{" "}
-          Add to Cart
-        </button>
+        <AddToCart food={food} onAddFood={onAddFood} />
       )}
 
       <small className="mt-4 text-[16px] font-medium text-rose-500">
@@ -317,6 +311,18 @@ function AddFood({ selectedFood, increment, decrement }) {
         />
       </button>
     </div>
+  );
+}
+
+function AddToCart({ food, onAddFood }) {
+  return (
+    <button
+      className="mx-auto -mt-6 flex flex-wrap gap-2 rounded-full border-2 border-rose-300 bg-white px-6 py-3 font-semibold text-rose-900"
+      onClick={() => onAddFood(food)}
+    >
+      <img src={cartIcon} alt="Add to cart icon" className="scale-110" /> Add to
+      Cart
+    </button>
   );
 }
 
