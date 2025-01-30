931f357b4cb73633876be4e69778767669c3237e
Abaljerind <abaljerinda@gmail.com>
2025-01-30 22:30:33 +0700

    Create component EmptyCartList to display if cart not have food inside

--- src/App.jsx
+++ src/App.jsx
@@ -107,17 +107,7 @@ function App() {
           onDeleteFood={handleDeleteFood}
         />
       ) : (
-        <div className="font-red-hat mx-auto mt-6 mb-6 flex w-[335px] flex-col justify-center rounded-2xl bg-white">
-          <h3 className="text-reds mt-7 ml-6 text-2xl font-bold">
-            Your Cart (0)
-          </h3>
-          <div className="mt-8 mb-6 flex flex-col items-center justify-center">
-            <img src={emptyCart} alt="Illustration of Empty Cart" />
-            <p className="mt-3 text-[16px] font-semibold text-rose-500">
-              Your added items will appear here
-            </p>
-          </div>
-        </div>
+        <EmptyCartList />
       )}
 
       {/* <div className="font-red-hat fixed bottom-0 z-[999] flex h-full max-h-[85vh] w-full flex-col items-start overflow-y-scroll rounded-2xl bg-white px-5 py-8">
@@ -328,6 +318,20 @@ function CartList({ totalFood, totalPrice, cart, onDeleteFood }) {
       <button className="bg-reds mx-auto mb-6 w-[85%] rounded-full py-4 font-semibold text-white">
         Confirm Order
       </button>
+    </div>
+  );
+}
+
+function EmptyCartList() {
+  return (
+    <div className="font-red-hat mx-auto mt-6 mb-6 flex w-[335px] flex-col justify-center rounded-2xl bg-white">
+      <h3 className="text-reds mt-7 ml-6 text-2xl font-bold">Your Cart (0)</h3>
+      <div className="mt-8 mb-6 flex flex-col items-center justify-center">
+        <img src={emptyCart} alt="Illustration of Empty Cart" />
+        <p className="mt-3 text-[16px] font-semibold text-rose-500">
+          Your added items will appear here
+        </p>
+      </div>
     </div>
   );
 }
