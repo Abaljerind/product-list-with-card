be1d59cb3c8cb90880999ed8450667444ca79cd1
Abaljerind <abaljerinda@gmail.com>
2025-01-31 22:25:43 +0700

    Paste codes from component Main and some functions
    Send state menu and cart to component Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,78 @@
+import { useEffect, useState } from "react";
 import Main from "./pages/Main";
+
+import foodData from "../data.json";
 
 document.body.style.backgroundColor = "rgb(254, 250, 247)";
 function App() {
+  const [menu, setMenu] = useState(foodData);
+  const [cart, setCart] = useState([]);
+
+  function handleAddFood(food) {
+    const id = `${food.name}-${food.price}`;
+
+    const newFood = {
+      id,
+      name: food.name,
+      price: food.price,
+      quantity: 1,
+    };
+
+    const existingFood = cart.find((item) => item.id === newFood.id);
+    if (existingFood) {
+      setCart((prevCart) =>
+        prevCart.map((item) => {
+          return item.id === id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        }),
+      );
+    } else {
+      setCart((prevCart) => [...prevCart, newFood]);
+    }
+  }
+
+  function increment(food) {
+    setCart((prevCart) =>
+      prevCart.map((item) => {
+        return item.id === food.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item;
+      }),
+    );
+  }
+
+  function decrement(food) {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) => {
+          return item.id === food.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item;
+        })
+        .filter((item) => item.quantity > 0),
+    );
+  }
+
+  function handleDeleteFood(food) {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== food.id));
+  }
+
+  useEffect(() => {
+    console.table(cart);
+  }, [cart]);
+
   return (
     <>
       {/* <div className="fixed top-0 right-0 left-0 z-[998] bg-black/50"> */}
-      <Main />
+      <Main
+        menu={menu}
+        cart={cart}
+        onAddFood={handleAddFood}
+        increment={increment}
+        decrement={decrement}
+        onDeleteFood={handleDeleteFood}
+      />
       {/* </div> */}
     </>
   );
