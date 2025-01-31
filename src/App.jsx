3ebc5a4ab51afdcaebc1171c538a8edcc67e7e22
Abaljerind <abaljerinda@gmail.com>
2025-01-31 22:12:15 +0700

    Invoke Main and delete some unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,16 @@
-import { useEffect, useState } from "react";
-import foodData from "../data.json";
-
 import orderSuccess from "/images/icon-order-confirmed.svg";
 import tiramisu from "/images/image-tiramisu-thumbnail.jpg";
 import vanillaBean from "/images/image-creme-brulee-thumbnail.jpg";
 import vanillaPanna from "/images/image-panna-cotta-thumbnail.jpg";
 
-import MenuFood from "./components/MenuFood";
-import CartSummary from "./components/CartSummary";
-import { Title } from "./components/Title";
+import Main from "./pages/Main";
 
 document.body.style.backgroundColor = "rgb(254, 250, 247)";
 function App() {
-  const [menu, setMenu] = useState(foodData);
-  const [cart, setCart] = useState([]);
-
-  function handleAddFood(food) {
-    const id = `${food.name}-${food.price}`;
-
-    const newFood = {
-      id,
-      name: food.name,
-      price: food.price,
-      quantity: 1,
-    };
-
-    const existingFood = cart.find((item) => item.id === newFood.id);
-    if (existingFood) {
-      setCart((prevCart) =>
-        prevCart.map((item) => {
-          return item.id === id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item;
-        }),
-      );
-    } else {
-      setCart((prevCart) => [...prevCart, newFood]);
-    }
-  }
-
-  function increment(food) {
-    setCart((prevCart) =>
-      prevCart.map((item) => {
-        return item.id === food.id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item;
-      }),
-    );
-  }
-
-  function decrement(food) {
-    setCart((prevCart) =>
-      prevCart
-        .map((item) => {
-          return item.id === food.id
-            ? { ...item, quantity: item.quantity - 1 }
-            : item;
-        })
-        .filter((item) => item.quantity > 0),
-    );
-  }
-
-  const totalFood = cart.reduce((acc, { ...curr }) => acc + curr.quantity, 0);
-  const totalPrice = cart.reduce(
-    (acc, { ...curr }) => acc + curr.quantity * curr.price,
-    0,
-  );
-
-  function handleDeleteFood(food) {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== food.id));
-  }
-
-  useEffect(() => {
-    console.table(cart);
-  }, [cart]);
-
   return (
     <>
       {/* <div className="fixed top-0 right-0 left-0 z-[998] bg-black/50"> */}
-      <Title />
-      <MenuFood
-        menu={menu}
-        cart={cart}
-        onAddFood={handleAddFood}
-        increment={increment}
-        decrement={decrement}
-      />
-
-      <CartSummary
-        totalFood={totalFood}
-        totalPrice={totalPrice}
-        cart={cart}
-        onDeleteFood={handleDeleteFood}
-      />
+      <Main />
 
       {/* <div className="font-red-hat fixed bottom-0 z-[999] flex h-full max-h-[85vh] w-full flex-col items-start overflow-y-scroll rounded-2xl bg-white px-5 py-8">
           <img src={orderSuccess} alt="icon order confirmation" />
