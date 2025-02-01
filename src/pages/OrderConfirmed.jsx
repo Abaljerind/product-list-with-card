9caa4a972ed6fee838f431e6f144eb22ab8c2f71
Abaljerind <abaljerinda@gmail.com>
2025-02-01 14:51:47 +0700

    Change static data with dynamic data from state cart
    OrderConfirmed receive cart and totalPrice as props
    Mapping state cart with div tag
    Change value of Order Total with data from totalPrice

--- src/pages/OrderConfirmed.jsx
+++ src/pages/OrderConfirmed.jsx
@@ -1,9 +1,6 @@
 import orderSuccess from "/images/icon-order-confirmed.svg";
-import tiramisu from "/images/image-tiramisu-thumbnail.jpg";
-import vanillaBean from "/images/image-creme-brulee-thumbnail.jpg";
-import vanillaPanna from "/images/image-panna-cotta-thumbnail.jpg";
 
-export default function OrderConfirmed() {
+export default function OrderConfirmed({ cart, totalPrice }) {
   return (
     <div className="font-red-hat fixed bottom-0 z-[999] flex h-full max-h-[85vh] w-full flex-col items-start overflow-y-scroll rounded-2xl bg-white px-5 py-8">
       <img src={orderSuccess} alt="icon order confirmation" />
@@ -16,62 +13,43 @@ export default function OrderConfirmed() {
 
       {/* food list */}
       <div className="my-6 w-full rounded-lg bg-rose-50 px-4 py-6">
-        <div className="mb-4 flex w-full items-center border-b border-rose-100 pb-4">
-          <img
-            src={tiramisu}
-            alt="classic tiramisu"
-            className="h-12 w-12 rounded-sm"
-          />
-          <div className="ml-4 flex flex-col">
-            <p className="text-sm font-semibold text-rose-900">
-              Classic Tiramissu
-            </p>
-            <small className="mt-2 text-sm">
-              <span className="text-reds font-semibold">1x</span>
-              <span className="ml-4 font-medium text-rose-400">@ $5.50</span>
-            </small>
-          </div>
-          <p className="ml-auto text-sm font-semibold text-rose-500">$5.50</p>
-        </div>
-        <div className="mb-4 flex w-full items-center border-b border-rose-100 pb-4">
-          <img
-            src={vanillaBean}
-            alt="vanilla bean creme"
-            className="h-12 w-12 rounded-sm"
-          />
-          <div className="ml-4 flex flex-col">
-            <p className="text-sm font-semibold text-rose-900">
-              Vanilla Bean Creme B...
-            </p>
-            <small className="mt-2 text-sm">
-              <span className="text-reds font-semibold">4x</span>
-              <span className="ml-4 font-medium text-rose-400">@ $7.00</span>
-            </small>
-          </div>
-          <p className="ml-auto text-sm font-semibold text-rose-500">$28.00</p>
-        </div>
-        <div className="mb-4 flex w-full items-center border-b border-rose-100 pb-4">
-          <img
-            src={vanillaPanna}
-            alt="vanilla panna"
-            className="h-12 w-12 rounded-sm"
-          />
-          <div className="ml-4 flex flex-col">
-            <p className="text-sm font-semibold text-rose-900">
-              Vanilla Panna Cotta
-            </p>
-            <small className="mt-2 text-sm">
-              <span className="text-reds font-semibold">2x</span>
-              <span className="ml-4 font-medium text-rose-400">@ $6.50</span>
-            </small>
-          </div>
-          <p className="ml-auto text-sm font-semibold text-rose-500">$13.00</p>
-        </div>
+        {cart.map((food, index) => {
+          return (
+            <div
+              className="mb-4 flex w-full items-center border-b border-rose-100 pb-4"
+              key={index + 1}
+            >
+              <img
+                src={food.imageThumbnail}
+                alt={food.name}
+                className="h-12 w-12 rounded-sm"
+              />
+              <div className="ml-4 flex flex-col">
+                <p className="text-sm font-semibold text-rose-900">
+                  {food.name}
+                </p>
+                <small className="mt-2 text-sm">
+                  <span className="text-reds font-semibold">
+                    {food.quantity}x
+                  </span>
+                  <span className="ml-4 font-medium text-rose-400">
+                    @ ${Number(food.price).toFixed(2)}
+                  </span>
+                </small>
+              </div>
+              <p className="ml-auto text-sm font-semibold text-rose-500">
+                ${Number(food.price * food.quantity).toFixed(2)}
+              </p>
+            </div>
+          );
+        })}
 
         {/* total and button */}
         <div className="mx-auto flex items-center justify-between">
           <p className="text-sm font-semibold text-rose-500">Order Total</p>
-          <p className="text-2xl font-bold text-rose-900">$46.50</p>
+          <p className="text-2xl font-bold text-rose-900">
+            ${Number(totalPrice).toFixed(2)}
+          </p>
         </div>
       </div>
 
