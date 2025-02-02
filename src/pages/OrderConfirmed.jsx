641738f3b7485280b5099e5135ae40aeb1785d73
Abaljerind <abaljerinda@gmail.com>
2025-02-02 18:29:35 +0700

    Add styles to tag parent and it's children elements for breakpoint md
    Add styles to parent div, small, div for food list, img,
    Tag p for food name, quantity, total price of a food and order total
    And last tag button

--- src/pages/OrderConfirmed.jsx
+++ src/pages/OrderConfirmed.jsx
@@ -2,17 +2,17 @@ import orderSuccess from "/images/icon-order-confirmed.svg";
 
 export default function OrderConfirmed({ cart, totalPrice, onNewOrder }) {
   return (
-    <div className="font-red-hat fixed bottom-0 z-[999] flex h-full max-h-[85vh] w-full flex-col items-start overflow-y-scroll rounded-2xl bg-white px-5 py-8">
+    <div className="font-red-hat md: fixed bottom-0 z-[999] flex h-full max-h-[85vh] w-full flex-col items-start overflow-y-scroll rounded-2xl bg-white px-5 py-8 md:left-[50%] md:w-[80%] md:-translate-x-[50%]">
       <img src={orderSuccess} alt="icon order confirmation" />
       <h1 className="mt-4 text-5xl leading-14 font-bold text-rose-900">
         Order Confirmed
       </h1>
-      <small className="mt-1 text-[16px] font-medium text-rose-500">
+      <small className="mt-1 text-[16px] font-medium text-rose-500 md:pl-2 md:text-lg">
         We hope you enjoy your food!
       </small>
 
       {/* food list */}
-      <div className="my-6 w-full rounded-lg bg-rose-50 px-4 py-6">
+      <div className="my-6 w-full rounded-lg bg-rose-50 px-4 py-6 md:mx-auto md:w-[90%]">
         {cart.map((food, index) => {
           return (
             <div
@@ -22,13 +22,13 @@ export default function OrderConfirmed({ cart, totalPrice, onNewOrder }) {
               <img
                 src={food.imageThumbnail}
                 alt={food.name}
-                className="h-12 w-12 rounded-sm"
+                className="h-12 w-12 rounded-sm md:h-14 md:w-14 md:rounded-md"
               />
               <div className="ml-4 flex flex-col">
-                <p className="text-sm font-semibold text-rose-900">
+                <p className="text-sm font-semibold text-rose-900 md:text-[16px]">
                   {food.name}
                 </p>
-                <small className="mt-2 text-sm">
+                <small className="mt-2 text-sm md:text-[16px]">
                   <span className="text-reds font-semibold">
                     {food.quantity}x
                   </span>
@@ -37,7 +37,7 @@ export default function OrderConfirmed({ cart, totalPrice, onNewOrder }) {
                   </span>
                 </small>
               </div>
-              <p className="ml-auto text-sm font-semibold text-rose-500">
+              <p className="ml-auto text-sm font-semibold text-rose-500 md:text-[16px]">
                 ${Number(food.price * food.quantity).toFixed(2)}
               </p>
             </div>
@@ -46,15 +46,17 @@ export default function OrderConfirmed({ cart, totalPrice, onNewOrder }) {
 
         {/* total and button */}
         <div className="mx-auto flex items-center justify-between">
-          <p className="text-sm font-semibold text-rose-500">Order Total</p>
-          <p className="text-2xl font-bold text-rose-900">
+          <p className="text-sm font-semibold text-rose-500 md:text-lg">
+            Order Total
+          </p>
+          <p className="text-2xl font-bold text-rose-900 md:text-3xl">
             ${Number(totalPrice).toFixed(2)}
           </p>
         </div>
       </div>
 
       <button
-        className="bg-reds mx-auto mb-6 w-full rounded-full py-4 font-semibold text-white"
+        className="bg-reds mx-auto mb-6 w-full rounded-full py-4 font-semibold text-white md:w-[90%]"
         onClick={onNewOrder}
       >
         Start New Order
