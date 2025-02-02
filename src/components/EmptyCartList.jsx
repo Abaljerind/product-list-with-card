1e93a574716e9f5f3e4bc67da12264f481c43c87
Abaljerind <abaljerinda@gmail.com>
2025-02-02 18:30:07 +0700

    Add style to div for breakpoint md

--- src/components/EmptyCartList.jsx
+++ src/components/EmptyCartList.jsx
@@ -2,7 +2,7 @@ import emptyCart from "/images/illustration-empty-cart.svg";
 
 export default function EmptyCartList() {
   return (
-    <div className="font-red-hat mx-auto mt-6 mb-6 flex w-[335px] flex-col justify-center rounded-2xl bg-white">
+    <div className="font-red-hat mx-auto mt-6 mb-6 flex w-[335px] flex-col justify-center rounded-2xl bg-white md:rounded-xl">
       <h3 className="text-reds mt-7 ml-6 text-2xl font-bold">Your Cart (0)</h3>
       <div className="mt-8 mb-6 flex flex-col items-center justify-center">
         <img src={emptyCart} alt="Illustration of Empty Cart" />
